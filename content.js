9b19573dfd3b1866f3092dedd36cea06b686e3d9
jeremyguillermin <85474922+jeremyguillermin@users.noreply.github.com>
2023-04-13 00:03:56 +0200

    Add files via upload

--- content.js
+++ content.js
@@ -1,16 +1,31 @@
+// Facebook
 let emojiClass = ".x1kky2od, img[alt^='\\uD83E\\uDD']";
-
 function removeEmojis() {
     let emojis = document.querySelectorAll(emojiClass);
     for (let i = 0; i < emojis.length; i++) {
         emojis[i].parentNode.removeChild(emojis[i]);
     }
 }
-
 var emojis = document.querySelectorAll('img[alt*="🙂"]');
 for (var i = 0; i < emojis.length; i++) {
   emojis[i].parentNode.removeChild(emojis[i]);
 }
 
+// Twitter
+function removeEmojis() {
+  const emojis = document.querySelectorAll('img[src*="/emoji/"]');
+  emojis.forEach(emoji => {
+    const emojiParent = emoji.parentElement;
+    if (emojiParent !== null) {
+      const emojiText = emoji.alt;
+      emojiParent.textContent = emojiParent.textContent.replace(emojiText, '');
+    }
+  });
+}
+removeEmojis();
 
+// Linkendin
+
+// ##################################
+removeEmojis(); // Exe function 
 setInterval(removeEmojis, 1000);
