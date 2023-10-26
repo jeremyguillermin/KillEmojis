46a3b5c387e04653cea33d37636191868b477d5f
Jérémy Guillermin <85474922+jeremyguillermin@users.noreply.github.com>
2023-10-26 15:15:49 +0200

    Add files via upload

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// Définition des règles pour chaque plateforme
+// Configuration pour chaque plateforme
 const platforms = {
   twitter: {
     emojiSelectors: 'img[src*="/emoji/"]',
@@ -14,16 +14,36 @@ const platforms = {
   },
 };
 
-// Fonction pour supprimer les emojis
+// Fonction générique pour supprimer les emojis
 function removeEmojis(platform) {
-  const { emojiSelectors, replacement } = platforms[platform];
+  const config = platforms[platform];
+  if (!config) {
+    console.error(`Configuration non trouvée pour la plateforme "${platform}"`);
+    return;
+  }
+  const { emojiSelectors, replacement } = config;
   const emojis = document.querySelectorAll(emojiSelectors);
+  const docFragment = document.createDocumentFragment();
+  
   emojis.forEach(emoji => {
     const parent = emoji.parentElement;
-    if (parent !== null) {
+    if (parent) {
       const newContent = typeof replacement === 'function' ? replacement(emoji, parent) : replacement;
       const newNode = document.createTextNode(newContent);
-      parent.replaceChild(newNode, emoji);
+      docFragment.appendChild(newNode);
+      parent.replaceChild(docFragment, emoji);
+    }
+  });
+}
+
+// Fonction pour supprimer les emojis sur Bluesky
+function removeEmojisBluesky() {
+  const posts = document.querySelectorAll('[data-testid="postText"]');
+  posts.forEach(post => {
+    for (let node of post.childNodes) {
+      if (node.nodeType === 3) {  // Node.TEXT_NODE
+        node.textContent = node.textContent.replace(/\p{Emoji}/gu, '');
+      }
     }
   });
 }
@@ -38,8 +58,24 @@ function removeSponsoredPosts() {
   });
 }
 
-// Appel des fonctions à intervalles réguliers
-setInterval(() => removeEmojis('twitter'), 500);
-setInterval(() => removeEmojis('facebook'), 1000);
-setInterval(() => removeEmojis('messenger'), 1000);
-setInterval(removeSponsoredPosts, 1000);
+// Écouteur d'événements pour les clics sur les publications
+document.addEventListener('click', event => {
+  const post = event.target.closest('[data-testid="postText"]');
+  if (post) {
+    removeEmojisBluesky();
+  }
+});
+
+// Instanciation et configuration d'un MutationObserver
+const observer = new MutationObserver(mutationsList => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList' || mutation.type === 'subtree') {
+      ['twitter', 'facebook', 'messenger'].forEach(platform => removeEmojis(platform));
+      removeEmojisBluesky();
+      removeSponsoredPosts();
+    }
+  }
+});
+
+// Démarrage de l'observation
+observer.observe(document.body, { childList: true, subtree: true });
